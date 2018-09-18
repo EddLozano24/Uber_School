0ffcfeeea1a66062c9cd7687f9dc0b733372e458
EddLozano24 <eddie.2497@gmail.com>
2018-09-18 00:39:08 -0500

    Ubicacion y ruta

--- app/src/main/java/com/example/eddie/uber_school/MapsActivity.java
+++ app/src/main/java/com/example/eddie/uber_school/MapsActivity.java
@@ -1,14 +1,23 @@
 package com.example.eddie.uber_school;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.example.eddie.uber_school.Objetos.References;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -17,7 +26,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.activity_maps2);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -38,9 +47,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker
-        LatLng actual = new LatLng(26, -100);
-        mMap.addMarker(new MarkerOptions().position(actual).title("Marker Actual"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(actual));
+        Intent inew = getIntent();
+        Bundle b = inew.getExtras();
+        final String nombre = (String) b.get("NOMBRE");
+
+        final DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("alumnos/"+nombre);
+
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Double lat = dataSnapshot.child("Latitud").getValue(Double.class);
+                Double lon = dataSnapshot.child("Longitud").getValue(Double.class);
+                LatLng sydney = new LatLng(lat, lon);
+                mMap.addMarker(new MarkerOptions().position(sydney).title("Ubicacion de "+nombre));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        // Add a marker in Sydney and move the camera
     }
 }
