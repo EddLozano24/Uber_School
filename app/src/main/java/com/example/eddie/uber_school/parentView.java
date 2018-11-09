d7936b71222e2b6b885cb1b458e0d31e0a36e659
DIR\adrian.briano.garcia <adrian_briano97@hotmail.com>
2018-11-08 23:50:18 -0600

    Template inicial de la vista del alumno

---
+++ app/src/main/java/com/example/eddie/uber_school/parentView.java
@@ -0,0 +1,13 @@
+package com.example.eddie.uber_school;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class parentView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parent_view);
+    }
+}
